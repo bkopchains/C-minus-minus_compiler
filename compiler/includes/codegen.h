0359750d5b71003f9deb9ca2750dd0a548ce8f9b
barrv <barrv@barrv-mbp.fios-router.home>
2016-05-18 12:10:15 -0400

    adding starter code for project 3

---
+++ compiler/includes/codegen.h
@@ -0,0 +1,11 @@
+#ifndef _CODEGEN_H
+#define _CODEGEN_H
+
+
+// you likely will need to include other 
+// header files from your compiler here
+#include "parser.h"
+
+// add all definitions exported by your code gen modules here
+
+#endif
